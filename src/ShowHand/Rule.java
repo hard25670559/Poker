30e826932b1eb0df440e1e42e8c90b6a35ba9180
unknown <>
2016-04-13 01:49:27 +0800

    增加判斷是否為同花規則

--- src/ShowHand/Rule.java
+++ src/ShowHand/Rule.java
@@ -9,16 +9,29 @@ public class Rule {
 		this.cards = cards;
 	}
 	
-	private Type test() {
-		for (int i=0 ; i<this.cards.length ; i++) {
-			
+	/**
+	 * 判斷是否為同花
+	 * 
+	 * @return 回傳是或否
+	 */
+	private boolean isFlush() {
+		boolean allSame = true;
+		Card tmp = null;
+		for (Card card : this.cards) {
+			if (tmp != null) {
+				if (card.getSuit() != tmp.getSuit())
+					return false;
+			} else {
+				tmp = card;
+			}
 		}
 		
-		return Type.FLUSH;
+		return true;
 	}
 	
 	public static void main(String[] args) {
-		Rule r = new Rule(Card.CLUB_EIGHT, Card.HEART_EIGHT, Card.DIAMOND_EIGHT, Card.SPADE_EIGHT, Card.DIAMOND_SIX);
+//		Rule r = new Rule(Card.CLUB_EIGHT, Card.HEART_EIGHT, Card.DIAMOND_EIGHT, Card.SPADE_EIGHT, Card.DIAMOND_SIX);
+		Rule r = new Rule(Card.CLUB_EIGHT, Card.DIAMOND_FOUR, Card.DIAMOND_EIGHT, Card.DIAMOND_SIX, Card.DIAMOND_TEN);
 		
 		System.out.println(r.test());
 		
