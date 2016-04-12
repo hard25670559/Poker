e766fc604b7fac09ef676493c248a2b6d2708176
unknown <>
2016-04-13 02:03:38 +0800

    增加判斷是否為"同花", "順子", "同花順", "鐵扇"的方法，"順子", "鐵扇"的詳細方法還未定義

--- src/ShowHand/Rule.java
+++ src/ShowHand/Rule.java
@@ -10,30 +10,64 @@ public class Rule {
 	}
 	
 	/**
+	 * 判斷是否為同花順
+	 * 
+	 * @return 回傳是或否
+	 */
+	public boolean isStraightFlush() {
+		return this.isStraight() && this.isFlush();
+	}
+	
+	/**
 	 * 判斷是否為同花
 	 * 
 	 * @return 回傳是或否
 	 */
-	private boolean isFlush() {
-		boolean allSame = true;
-		Card tmp = null;
+	public boolean isFlush() {
+		Card tmp = null;	//基準暫存
+		
 		for (Card card : this.cards) {
 			if (tmp != null) {
 				if (card.getSuit() != tmp.getSuit())
-					return false;
+					return false;	//不相同就直接回傳false
 			} else {
-				tmp = card;
+				tmp = card;	//指定一張作為基準
 			}
 		}
+		
+		return true;	//上訴流程跑完即為同花
+	}
+	
+	/**
+	 * 判斷是否為順子
+	 * 
+	 * @return 回傳是或否
+	 */
+	public boolean isStraight() {
+		
+		
+		
+		return true;
+	}
+	
+	/**
+	 * 判斷是否為鐵扇
+	 * 
+	 * @return 回傳是或否
+	 */
+	public boolean isFourOfAKind() {
+		
+		
 		
 		return true;
 	}
 	
 	public static void main(String[] args) {
 //		Rule r = new Rule(Card.CLUB_EIGHT, Card.HEART_EIGHT, Card.DIAMOND_EIGHT, Card.SPADE_EIGHT, Card.DIAMOND_SIX);
-		Rule r = new Rule(Card.CLUB_EIGHT, Card.DIAMOND_FOUR, Card.DIAMOND_EIGHT, Card.DIAMOND_SIX, Card.DIAMOND_TEN);
+//		Rule r = new Rule(Card.CLUB_EIGHT, Card.DIAMOND_FOUR, Card.DIAMOND_EIGHT, Card.DIAMOND_SIX, Card.DIAMOND_TEN);
+		Rule r = new Rule(Card.DIAMOND_TWELVE, Card.DIAMOND_FOUR, Card.DIAMOND_EIGHT, Card.DIAMOND_SIX, Card.DIAMOND_TEN);
 		
-		System.out.println(r.test());
+		System.out.println(r.isFlush());
 		
 	}
 	
