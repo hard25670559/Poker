8a8efe2e1f6e67cacd048a9e3047ebc19296d35b
unknown <>
2016-04-12 23:55:25 +0800

    新增牌型類別

---
+++ src/ShowHandType.java
@@ -0,0 +1,19 @@
+public enum ShowHandType {
+	STRAIGHT_FLUSH(0), FOUR_OF_A_KIND(1), FULL_HOUSE(2), FLUSH(3), STRAIGHT(4), THREE_OF_A_KIND(5), TWO_PAIRS(6), ONE_PAIR(7), HIGH_CARD(8);
+	
+	private int code;
+
+	private ShowHandType(int code) {
+		this.code = code;
+	}
+	
+	public int getCode() {
+		return this.code;
+	}
+}
+
+class test1 {
+	public static void main(String[] args) {
+		
+	}
+}
