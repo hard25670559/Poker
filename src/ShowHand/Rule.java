6728b22cb850b765416fc926771133d6aaee59c6
unknown <>
2016-04-13 00:49:40 +0800

    制定規則測試模式

--- src/ShowHand/Rule.java
+++ src/ShowHand/Rule.java
@@ -3,14 +3,26 @@ package ShowHand;
 import Poker.Card;
 
 public class Rule {
-	private Card[] card = new Card[5];
+	private Card[] cards = new Card[5];
 	
-	private void test() {
-		for (int i=0 ; i<this.card.length ; i++) {
-			
-		}
+	public Rule(Card... cards) {
+		this.cards = cards;
 	}
 	
+	private Type test() {
+		for (int i=0 ; i<this.cards.length ; i++) {
+			
+		}
+		
+		return Type.FLUSH;
+	}
+	
+	public static void main(String[] args) {
+		Rule r = new Rule(Card.CLUB_EIGHT, Card.HEART_EIGHT, Card.DIAMOND_EIGHT, Card.SPADE_EIGHT, Card.DIAMOND_SIX);
+		
+		System.out.println(r.test());
+		
+	}
 	
 
 }
