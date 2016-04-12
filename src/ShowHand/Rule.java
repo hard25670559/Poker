1849e3927afe27a1411e602537d83ce58b34e426
unknown <>
2016-04-13 00:21:23 +0800

    梭哈規則未編輯完畢

--- src/ShowHand/Rule.java
+++ src/ShowHand/Rule.java
@@ -1,6 +1,15 @@
 package ShowHand;
 
+import Poker.Card;
+
 public class Rule {
+	private Card[] card = new Card[5];
+	
+	private void test() {
+		for (int i=0 ; i<this.card.length ; i++) {
+			
+		}
+	}
 	
 	
 
--- src/ShowHand/Type.java
+++ src/ShowHand/Type.java
@@ -1,4 +1,5 @@
 package ShowHand;
+
 public enum Type {
 	STRAIGHT_FLUSH(0), FOUR_OF_A_KIND(1), FULL_HOUSE(2), FLUSH(3), STRAIGHT(4), THREE_OF_A_KIND(5), TWO_PAIRS(6), ONE_PAIR(7), HIGH_CARD(8);
 	
