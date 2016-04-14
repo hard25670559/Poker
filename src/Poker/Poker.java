de7c0b0b0c63a3691d3a9373aedc3abacc00ddf6
hard25670559 <hard25670559@gmail.com>
2016-04-14 15:38:01 +0800

    新增撲克牌排序((依照牌點數排序

--- src/Poker/Poker.java
+++ src/Poker/Poker.java
@@ -1,4 +1,7 @@
 package Poker;
+
+import java.util.LinkedList;
+
 /**
  * 產生一副撲克牌
  * 
@@ -54,4 +57,48 @@ public class Poker {
 		this.poker = tmp;
 	}
 	
+	/**
+	 * 按照大小排序，小的在前，大的在後
+	 * 透過氣泡排序法
+	 * 
+	 * @param cards	被操作的牌組
+	 * @return	返回一個排序完的陣列
+	 */
+	public static Card[] numberSort(Card... cards) {
+		Card card_tmp = null;	//如果相鄰兩值必須互換時，所需用到的暫存
+		for (int i=0 ; i<cards.length ; i++) {
+			for (int j=0 ; j<cards.length-i-1 ; j++) {
+				if (cards[j].getNumber().getCode() > cards[j+1].getNumber().getCode()) {
+					card_tmp = cards[j+1];
+					cards[j+1] = cards[j];
+					cards[j] = card_tmp;
+				}
+			}
+		}
+		return cards;
+	}
+	
+	/**
+	 * 將一組牌照花色分類
+	 * 
+	 * @param cards 被操作的陣列
+	 * @return 返回一個分類完成的牌組
+	 */
+	public Card[] suitSort(Card... cards) {
+		LinkedList<Card> tmp = new LinkedList<>();
+		Card[] anser = new Card[cards.length];
+		for (Suit suit : Suit.values()) {
+			for (int index=0 ; index<cards.length ; index++) {
+				if (suit == cards[index].getSuit()) {
+					anser[index] = cards[index];
+				}
+			}
+		}
+		
+		return anser;
+	}
+	
+	public static Card[] typeSort() {
+		return null;
+	}
 }
